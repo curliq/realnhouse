0004273e015babaec6c816feb57f27e34c05e2a1
curlicue <joaosardinha18@hotmail.com>
2018-09-24 20:56:47 +0100

    fix

--- bot/index.js
+++ bot/index.js
@@ -13,7 +13,6 @@ console.log = function (body) {
 client.on("message", (message) => {
     log(message.content + " - on " + message.channel);
     if (message.channel.id === constants.CHANNEL_QUEUE ||
-        message.channel.id === constants.CHANNEL_LEADERBOARD ||
         message.channel.id === constants.CHANNEL_REGISTER)
 
         if (message.author.id !== constants.BOT_ID)
